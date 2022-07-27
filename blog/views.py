97e9e5adc95783a18ba2b020c7befda70a9e1131
kjdonahue <kjd88@cornell.edu>
2022-07-27 13:18:47 -0600

    Adding next/previous button to posts

--- blog/views.py
+++ blog/views.py
@@ -27,8 +27,25 @@ def blog(request):
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug=post, status=Post.Status.PUBLISHED)
+
+    # Calculate the next and previous posts
+    posts = Post.published.all()
+
+    # Find all posts before / after the current post
+    posts_after  = posts.filter(publish__gt=post.publish)
+    posts_before = posts.filter(publish__lt=post.publish)
+
+    next_post = posts_after.last()
+    prev_post = posts_before.first()
+
+    # If the post exists, find its URL.  Else just stay on the same page
+    next_post_url = post.get_absolute_url() + '#' if next_post == None else next_post.get_absolute_url()
+    prev_post_url = post.get_absolute_url() + '#' if prev_post == None else prev_post.get_absolute_url()
+
     return render(request, 'blog/post_detail.html', {
         'post': post,
+        'next_post_url': next_post_url,
+        'prev_post_url': prev_post_url,
         'nav_type': 'thin',
         'content_style': 'post-detail has-background'
     })
